8b5d713b12ec37269a06006c58e9c8de0d576583
Ritik-Patidar <ritikpatidar029@gmail.com>
2024-03-25 16:18:08 +0530

    remove unused files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,32 @@
 "use client";
-import "jsvectormap/dist/css/jsvectormap.css";
-import "flatpickr/dist/flatpickr.min.css";
 import "@/css/satoshi.css";
 import "@/css/style.css";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Loader from "@/components/common/Loader";
 import StoreProvider from "@/redux/StoreProvider";
+import Header from "@/components/Header";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
-  }, []);
+  const [loading] = useState<boolean>(false);
 
   return (
     <html lang="en">
       <body suppressHydrationWarning={true}>
         <StoreProvider>
-          <div className="dark:bg-boxdark-2 dark:text-bodydark">{loading ? <Loader /> : children}</div>
+          <div>
+            {loading ? (
+              <Loader />
+            ) : (
+              <>
+                <Header />
+                {children}
+              </>
+            )}
+          </div>
         </StoreProvider>
       </body>
     </html>
