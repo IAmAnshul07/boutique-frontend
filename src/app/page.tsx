d265df5aa4a36492939e8a84290936303d87b9d6
Ritik-Patidar <ritikpatidar029@gmail.com>
2024-03-29 22:13:56 +0530

    Update package.json and page.tsx, remove whiteBackground.png

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { Metadata } from "next";
 import Image from "next/image";
 import rightImage from "../asset/homepage/rightImage.png";
 import women from "../asset/homepage/women.png";
-import whiteBackground from "../asset/homepage/whiteBackgroound.png";
 
 export const metadata: Metadata = {
   title: "Fashion | Fashion Boutique",
@@ -12,19 +11,18 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-      <div className="top-container relative flex">
-        <div className="left-container flex-grow">
-          <Image src={whiteBackground.src} width={400} height={400} alt="Right Side Image" className="w-full h-[70%]" />
+      <div className="flex flex-row items-center justify-center h-125 relative">
+        <div className="box1 flex w-full h-125 absolute">
+          <div className="bg-white h-full w-1/2"></div>
+          <div className="bg-blue-500 h-full w-1/2 relative">
+            <Image src={rightImage.src} fill={true} alt="Right Side Image" className="w-full h-[70%] object-cover" />
+          </div>
         </div>
-        <div className="w-full middle-container absolute left-1/2 top-1/4 transform -translate-x-1/2 -translate-y-1/2 z-20 flex items-center justify-center">
-          <h1 className="text-center text-black text-[11rem] font-bold">NEW FASHION</h1>
+        <div className="box2 z-10 absolute">
+          <h1 className="text-center text-base-200 text-8xl lg:text-9xl xl:text-10xl font-bold">NEW FASHION</h1>
         </div>
-        <div className="middle-container absolute left-1/2 transform -translate-x-1/2 z-30">
-          <Image src={women.src} width={400} height={400} alt="Middle Image" />
-        </div>
-
-        <div className="right-container flex-grow">
-          <Image src={rightImage.src} width={600} height={600} alt="Right Side Image" className="w-full h-[70%]" />
+        <div className="box3 z-20">
+          <Image src={women.src} height={350} width={350} alt="Middle Image" className="" />
         </div>
       </div>
     </>
