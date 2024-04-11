a236a76965c8266209a75b695c2081c840fdbef3
IAmAnshul07 <anshulpatidar1298@gmail.com>
2024-04-11 09:17:20 +0530

    fix: added banner component to a;; three catagories

--- src/app/(catagories)/kids/page.tsx
+++ src/app/(catagories)/kids/page.tsx
@@ -1,17 +1,23 @@
 "use client";
 import React from "react";
 import Card from "@/components/card";
+import Banner from "@/components/banner";
 import { responseData } from "@/lib/women";
 
 const Women: React.FC<{}> = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center">
-      {responseData.map((product) => (
-        <div key={product.id} className="m-4">
-          <Card product={product} />
-        </div>
-      ))}
-    </div>
+    <>
+      <div>
+        <Banner />
+      </div>
+      <div className="flex flex-row flex-wrap justify-center">
+        {responseData.map((product) => (
+          <div key={product.id} className="m-4">
+            <Card product={product} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
