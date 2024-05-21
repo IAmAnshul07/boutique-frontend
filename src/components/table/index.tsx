8e012a14880d12e29ebd22239a7acd45c146144c
IAmAnshul07 <anshulpatidar1298@gmail.com>
2024-05-21 22:26:36 +0530

    fix/ error fixed regarding counts and result

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -14,6 +14,8 @@ interface TableProps {
 }
 
 const Table: React.FC<TableProps> = ({ columns, data, handleEdit, handleDelete }) => {
+  console.log("data ->", data);
+
   return (
     <div className="overflow-x-auto m-5">
       <table className="min-w-full divide-y divide-[#d3d4d7] border border-[#d3d4d7]">
@@ -32,29 +34,30 @@ const Table: React.FC<TableProps> = ({ columns, data, handleEdit, handleDelete }
           </tr>
         </thead>
         <tbody className="bg-white divide-[#d3d4d7]">
-          {data.map((row) => (
-            <tr key={row.id}>
-              {columns.map((column) => (
-                <td key={column.accessor} className="px-6 py-4 whitespace-nowrap">
-                  {column.render ? column.render(row) : row[column.accessor]}
-                </td>
-              ))}
-              {(handleEdit || handleDelete) && (
-                <td className="px-6 py-4 whitespace-nowrap">
-                  {handleEdit && (
-                    <button onClick={() => handleEdit(row)} className="btn btn-ghost btn-xs text-indigo-600 hover:text-indigo-900">
-                      Edit
-                    </button>
-                  )}
-                  {handleDelete && (
-                    <button onClick={() => handleDelete(row.id)} className="btn btn-ghost btn-xs text-red-600 hover:text-red-900">
-                      Delete
-                    </button>
-                  )}
-                </td>
-              )}
-            </tr>
-          ))}
+          {data &&
+            data?.map((row) => (
+              <tr key={row.id}>
+                {columns.map((column) => (
+                  <td key={column.accessor} className="px-6 py-4 whitespace-nowrap">
+                    {column.render ? column.render(row) : row[column.accessor]}
+                  </td>
+                ))}
+                {(handleEdit || handleDelete) && (
+                  <td className="px-6 py-4 whitespace-nowrap">
+                    {handleEdit && (
+                      <button onClick={() => handleEdit(row)} className="btn btn-ghost btn-xs text-indigo-600 hover:text-indigo-900">
+                        Edit
+                      </button>
+                    )}
+                    {handleDelete && (
+                      <button onClick={() => handleDelete(row.id)} className="btn btn-ghost btn-xs text-red-600 hover:text-red-900">
+                        Delete
+                      </button>
+                    )}
+                  </td>
+                )}
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
