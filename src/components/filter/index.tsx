adf4e715fb24c2e14a13b9c8352889f7e9d5e17d
IAmAnshul07 <anshulpatidar1298@gmail.com>
2024-05-23 11:50:47 +0530

    refactored the code for a customhook to use filter as a parameter

--- src/components/filter/index.tsx
+++ src/components/filter/index.tsx
@@ -1,7 +1,8 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useGetCategoriesQuery } from "@/redux/services/category";
 import { useGetColorsQuery } from "@/redux/services/color";
+import useSearchFilterParam from "@/hooks/useFilterHook";
 
 interface Color {
   id: number;
@@ -14,122 +15,83 @@ const Filter = () => {
   const price = ["Rs. 59 to Rs. 500", "Rs. 500 to Rs. 1000", "Rs. 1000 to Rs. 2000", "Rs. 2000 to Rs. 5000"];
   const { data: categoriesData } = useGetCategoriesQuery();
   const { data: colorsData } = useGetColorsQuery();
-
-  const [searchParams, setSearchParams] = useState(new URLSearchParams(typeof window !== "undefined" ? window.location.search : ""));
-
-  const updateURLParams = (key: string, values: string[] | string) => {
-    const params = new URLSearchParams(searchParams);
-    if ((values as string[]).length) {
-      params.set(key, (values as string[]).join(","));
-    } else {
-      params.delete(key);
-    }
-    setSearchParams(params);
-    window.history.replaceState({}, "", `?${params.toString()}`);
-  };
-
-  const handlePriceChange = (priceValue: string) => {
-    const selectedPrices = searchParams.get("prices")?.split(",") || [];
-    const newPrices = selectedPrices.includes(priceValue) ? selectedPrices.filter((value) => value !== priceValue) : [...selectedPrices, priceValue];
-    updateURLParams("prices", newPrices);
-  };
-
-  const handleCategoryChange = (categoryValue: string) => {
-    const selectedCategories = searchParams.get("categories")?.split(",") || [];
-    const newCategories = selectedCategories.includes(categoryValue)
-      ? selectedCategories.filter((value) => value !== categoryValue)
-      : [...selectedCategories, categoryValue];
-    updateURLParams("categories", newCategories);
-  };
-
-  const handleTypeChange = (typeValue: string) => {
-    const selectedTypes = searchParams.get("types")?.split(",") || [];
-    const newTypes = selectedTypes.includes(typeValue) ? selectedTypes.filter((value) => value !== typeValue) : [...selectedTypes, typeValue];
-    updateURLParams("types", newTypes);
-  };
-
-  const handleColorChange = (colorValue: string) => {
-    const selectedColors = searchParams.get("colors")?.split(",") || [];
-    const newColors = selectedColors.includes(colorValue) ? selectedColors.filter((value) => value !== colorValue) : [...selectedColors, colorValue];
-    updateURLParams("colors", newColors);
-  };
-
-  useEffect(() => {
-    setSearchParams(new URLSearchParams(typeof window !== "undefined" ? window.location.search : ""));
-  }, []);
+  const { getParamValues, handleItemChange, clearAllParams } = useSearchFilterParam();
 
   return (
-    <>
-      <div className="flex flex-col border-r-2 border-[#e8e9ea] mx-8 mt-5">
-        <div>
+    <div className="flex flex-col border-r-2 border-[#e8e9ea] mx-8 mt-5">
+      <div>
+        <div className="flex justify-between items-center">
           <h1 className="text-md">FILTERS</h1>
-          <div className="divider divider-end w-11/12"></div>
-          <h1 className="text-md">TYPE</h1>
-          {categoryFilter.map((category, index) => (
-            <label key={index} className="flex items-center space-x-2">
-              <input
-                type="checkbox"
-                value={index}
-                className="checkbox checkbox-sm"
-                checked={new URLSearchParams(searchParams).get("types")?.split(",").includes(category) || false}
-                onChange={() => handleTypeChange(category)}
-              />
-              <span>{category}</span>
-            </label>
-          ))}
+          <button className="btn btn-xs mr-4" onClick={clearAllParams}>
+            Clear filter
+          </button>
         </div>
         <div className="divider divider-end w-11/12"></div>
-        <div>
-          <h1 className="text-md">CATEGORIES</h1>
-          {categoriesData?.data?.map((category: { id: string; name: string }) => (
-            <label key={category.id} className="flex items-center space-x-2">
-              <input
-                type="checkbox"
-                value={category.id}
-                className="checkbox checkbox-sm"
-                checked={new URLSearchParams(searchParams).get("categories")?.split(",").includes(category.name) || false}
-                onChange={() => handleCategoryChange(category.name)}
-              />
-              <span>{category.name}</span>
-            </label>
-          ))}
-        </div>
-        <div className="divider divider-end w-11/12"></div>
-        <div>
-          <h1 className="text-md">PRICE</h1>
-          {price.map((amount, index) => (
-            <label key={index} className="flex items-center space-x-2">
-              <input
-                type="checkbox"
-                value={amount}
-                className="checkbox checkbox-sm"
-                checked={new URLSearchParams(searchParams).get("prices")?.split(",").includes(amount) || false}
-                onChange={() => handlePriceChange(amount)}
-              />
-              <span>{amount}</span>
-            </label>
-          ))}
-        </div>
-        <div className="divider divider-end w-11/12"></div>
-        <div>
-          <h1 className="text-md">COLOR</h1>
-          {colorsData &&
-            colorsData?.result?.map((color: Color) => (
-              <label key={color.id} className="flex items-center space-x-2">
-                <input
-                  type="checkbox"
-                  value={color.id}
-                  className="checkbox checkbox-sm"
-                  checked={new URLSearchParams(searchParams).get("colors")?.split(",").includes(color.name) || false}
-                  onChange={() => handleColorChange(color.name)}
-                />
-                <span className="w-4 h-4 rounded-full" style={{ backgroundColor: color.hex }}></span>
-                <span>{color.name}</span>
-              </label>
-            ))}
-        </div>
+        <h1 className="text-md">TYPE</h1>
+        {categoryFilter.map((category, index) => (
+          <label key={index} className="flex items-center space-x-2">
+            <input
+              type="checkbox"
+              value={category}
+              className="checkbox checkbox-sm"
+              checked={getParamValues("types").includes(category)}
+              onChange={() => handleItemChange("types", category)}
+            />
+            <span>{category}</span>
+          </label>
+        ))}
       </div>
-    </>
+      <div className="divider divider-end w-11/12"></div>
+      <div>
+        <h1 className="text-md">CATEGORIES</h1>
+        {categoriesData?.data?.map((category: { id: string; name: string }) => (
+          <label key={category.id} className="flex items-center space-x-2">
+            <input
+              type="checkbox"
+              value={category.name}
+              className="checkbox checkbox-sm"
+              checked={getParamValues("categories").includes(category.name)}
+              onChange={() => handleItemChange("categories", category.name)}
+            />
+            <span>{category.name}</span>
+          </label>
+        ))}
+      </div>
+      <div className="divider divider-end w-11/12"></div>
+      <div>
+        <h1 className="text-md">PRICE</h1>
+        {price.map((amount, index) => (
+          <label key={index} className="flex items-center space-x-2">
+            <input
+              type="checkbox"
+              value={amount}
+              className="checkbox checkbox-sm"
+              checked={getParamValues("prices").includes(amount)}
+              onChange={() => handleItemChange("prices", amount)}
+            />
+            <span>{amount}</span>
+          </label>
+        ))}
+      </div>
+      <div className="divider divider-end w-11/12"></div>
+      <div>
+        <h1 className="text-md">COLOR</h1>
+        {colorsData &&
+          colorsData?.result?.map((color: Color) => (
+            <label key={color.id} className="flex items-center space-x-2">
+              <input
+                type="checkbox"
+                value={color.name}
+                className="checkbox checkbox-sm"
+                checked={getParamValues("colors").includes(color.name)}
+                onChange={() => handleItemChange("colors", color.name)}
+              />
+              <span className="w-4 h-4 rounded-full" style={{ backgroundColor: color.hex }}></span>
+              <span>{color.name}</span>
+            </label>
+          ))}
+      </div>
+    </div>
   );
 };
 
---
+++ src/hooks/useFilterHook/index.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { useCallback } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+
+const useSearchFilterParam = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const getParamValues = useCallback(
+    (key: string) => {
+      return searchParams.get(key)?.split(",") || [];
+    },
+    [searchParams],
+  );
+
+  const updateURLParams = useCallback(
+    (key: string, values: string[]) => {
+      const params = new URLSearchParams(searchParams.toString());
+      if (values.length) {
+        params.set(key, values.join(","));
+      } else {
+        params.delete(key);
+      }
+      const queryString = `?${params.toString()}`;
+      router.replace(queryString);
+    },
+    [searchParams, router],
+  );
+
+  const handleItemChange = useCallback(
+    (key: string, value: string) => {
+      const currentValues = getParamValues(key);
+      const newValues = currentValues.includes(value) ? currentValues.filter((item) => item !== value) : [...currentValues, value];
+      updateURLParams(key, newValues);
+    },
+    [getParamValues, updateURLParams],
+  );
+
+  const clearAllParams = useCallback(() => {
+    router.replace("?");
+  }, [router]);
+
+  return {
+    searchParams,
+    getParamValues,
+    handleItemChange,
+    clearAllParams,
+  };
+};
+
+export default useSearchFilterParam;
