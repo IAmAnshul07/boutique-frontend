85fab059f449cb5f20e835f32d7265ebd469bccb
IAmAnshul07 <anshulpatidar1298@gmail.com>
2024-06-15 18:18:26 +0530

    refactor:next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< Updated upstream
-const nextConfig = {};
-=======
 const nextConfig = {
   images: {
     domains: ["picsum.photos", "res.cloudinary.com", "rukminim2.flixcart.com"], //TODO: Remove this if you are not using picsum.photos
   },
 };
->>>>>>> Stashed changes
 
 export default nextConfig;
