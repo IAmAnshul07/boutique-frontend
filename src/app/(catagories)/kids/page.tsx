9500bd4db68333ed3ab0c52954d017bac53d84c2
IAmAnshul07 <anshulpatidar1298@gmail.com>
2024-06-18 12:03:03 +0530

    refactor: changed product view to grid

--- src/app/(catagories)/kids/page.tsx
+++ src/app/(catagories)/kids/page.tsx
@@ -7,9 +7,9 @@ import Link from "next/link";
 const Women: React.FC<{}> = () => {
   return (
     <>
-      <div className="flex flex-row flex-wrap">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
         {responseData.map((product) => (
-          <Link href={`product/${product.id}`} key={product.id} className="m-4">
+          <Link href={`product/${product.id}`} key={product.id}>
             <Card product={product} />
           </Link>
         ))}
