b6a1ba8a65a918c7a3973bf235153c2dd48734b7
IAmAnshul07 <anshulpatidar1298@gmail.com>
2024-07-06 18:45:30 +0530

    reduced right margin from filter component for consistance margin

--- src/components/filter/index.tsx
+++ src/components/filter/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { useGetCategoriesQuery } from "@/redux/services/category";
 import { useGetColorsQuery } from "@/redux/services/color";
 import useSearchFilterParam from "@/hooks/useSearchFilterParam";
@@ -12,6 +12,7 @@ interface Color {
 }
 
 const Filter = () => {
+  const [showFilters, setShowFilters] = useState(false);
   const categoryFilter = ["Women", "Men", "Kids"];
   const price = ["Rs. 59 to Rs. 500", "Rs. 500 to Rs. 1000", "Rs. 1000 to Rs. 2000", "Rs. 2000 to Rs. 5000"];
   const { data: categoriesData } = useGetCategoriesQuery();
@@ -19,96 +20,99 @@ const Filter = () => {
   const { data: occasionData } = useGetTagsQuery("");
   const { getParamValues, handleItemChange, clearAllParams } = useSearchFilterParam();
 
+  const toggleFilters = () => {
+    setShowFilters(!showFilters);
+  };
+
   return (
-    <div className="flex flex-col border-r-2 border-[#e8e9ea] mx-8 mt-5">
-      <div>
-        <div className="flex justify-between items-center">
-          <h1 className="text-md">FILTERS</h1>
-          <button className="btn btn-xs mr-4" onClick={clearAllParams}>
-            Clear filter
-          </button>
-        </div>
-        <div className="divider divider-end w-11/12"></div>
-        <h1 className="text-md">TYPE</h1>
-        {categoryFilter.map((category, index) => (
-          <label key={index} className="flex items-center space-x-2">
-            <input
-              type="checkbox"
-              value={category}
-              className="checkbox checkbox-sm"
-              checked={getParamValues("types").includes(category)}
-              onChange={() => handleItemChange("types", category)}
-            />
-            <span>{category}</span>
-          </label>
-        ))}
+    <div className="flex flex-col md:border-r-2 !mr-0 md:border-[#e8e9ea] mx-4 sm:mx-8 mt-5">
+      <div className="md:hidden mb-4">
+        <button className="btn btn-xs w-full" onClick={toggleFilters}>
+          {showFilters ? "Show Filters" : "Hide Filters"}
+        </button>
       </div>
-      <div className="divider divider-end w-11/12"></div>
-      <div>
-        <h1 className="text-md">CATEGORIES</h1>
-        {categoriesData?.data?.map((category: { id: string; name: string }) => (
-          <label key={category.id} className="flex items-center space-x-2">
-            <input
-              type="checkbox"
-              value={category.name}
-              className="checkbox checkbox-sm"
-              checked={getParamValues("categories").includes(category.name)}
-              onChange={() => handleItemChange("categories", category.name)}
-            />
-            <span>{category.name}</span>
-          </label>
-        ))}
-      </div>
-      <div className="divider divider-end w-11/12"></div>
-      <div>
-        <h1 className="text-md">OCCASIONS</h1>
-        {occasionData?.data?.map((occasion: { id: string; name: string }) => (
-          <label key={occasion.id} className="flex items-center space-x-2">
-            <input
-              type="checkbox"
-              value={occasion.name}
-              className="checkbox checkbox-sm"
-              checked={getParamValues("occasions").includes(occasion.name)}
-              onChange={() => handleItemChange("occasions", occasion.name)}
-            />
-            <span>{occasion.name}</span>
-          </label>
-        ))}
-      </div>
-      <div className="divider divider-end w-11/12"></div>
-      <div>
-        <h1 className="text-md">PRICE</h1>
-        {price.map((amount, index) => (
-          <label key={index} className="flex items-center space-x-2">
-            <input
-              type="checkbox"
-              value={amount}
-              className="checkbox checkbox-sm"
-              checked={getParamValues("prices").includes(amount)}
-              onChange={() => handleItemChange("prices", amount)}
-            />
-            <span>{amount}</span>
-          </label>
-        ))}
-      </div>
-      <div className="divider divider-end w-11/12"></div>
-      <div>
-        <h1 className="text-md">COLOR</h1>
-        {colorsData &&
-          colorsData?.result?.map((color: Color) => (
-            <label key={color.id} className="flex items-center space-x-2">
+      {showFilters || (
+        <div>
+          <div className="flex justify-between items-center">
+            <h1 className="text-md">FILTERS</h1>
+            <button className="btn btn-xs mr-2 sm:mr-4" onClick={clearAllParams}>
+              Clear filter
+            </button>
+          </div>
+          <div className="divider divider-end w-full sm:w-11/12"></div>
+          <h1 className="text-md">TYPE</h1>
+          {categoryFilter.map((category, index) => (
+            <label key={index} className="flex items-center space-x-2">
               <input
                 type="checkbox"
-                value={color.name}
+                value={category}
                 className="checkbox checkbox-sm"
-                checked={getParamValues("colors").includes(color.name)}
-                onChange={() => handleItemChange("colors", color.name)}
+                checked={getParamValues("types").includes(category)}
+                onChange={() => handleItemChange("types", category)}
               />
-              <span className="w-4 h-4 rounded-full" style={{ backgroundColor: color.hex }}></span>
-              <span>{color.name}</span>
+              <span>{category}</span>
             </label>
           ))}
-      </div>
+          <div className="divider divider-end w-full sm:w-11/12"></div>
+          <h1 className="text-md">CATEGORIES</h1>
+          {categoriesData?.data?.map((category: { id: string; name: string }) => (
+            <label key={category.id} className="flex items-center space-x-2">
+              <input
+                type="checkbox"
+                value={category.name}
+                className="checkbox checkbox-sm"
+                checked={getParamValues("categories").includes(category.name)}
+                onChange={() => handleItemChange("categories", category.name)}
+              />
+              <span>{category.name}</span>
+            </label>
+          ))}
+          <div className="divider divider-end w-full sm:w-11/12"></div>
+          <h1 className="text-md">OCCASIONS</h1>
+          {occasionData?.data?.map((occasion: { id: string; name: string }) => (
+            <label key={occasion.id} className="flex items-center space-x-2">
+              <input
+                type="checkbox"
+                value={occasion.name}
+                className="checkbox checkbox-sm"
+                checked={getParamValues("occasions").includes(occasion.name)}
+                onChange={() => handleItemChange("occasions", occasion.name)}
+              />
+              <span>{occasion.name}</span>
+            </label>
+          ))}
+          <div className="divider divider-end w-full sm:w-11/12"></div>
+          <h1 className="text-md">PRICE</h1>
+          {price.map((amount, index) => (
+            <label key={index} className="flex items-center space-x-2">
+              <input
+                type="checkbox"
+                value={amount}
+                className="checkbox checkbox-sm"
+                checked={getParamValues("prices").includes(amount)}
+                onChange={() => handleItemChange("prices", amount)}
+              />
+              <span>{amount}</span>
+            </label>
+          ))}
+          <div className="divider divider-end w-full sm:w-11/12"></div>
+          <h1 className="text-md">COLOR</h1>
+          {colorsData &&
+            colorsData?.result?.map((color: Color) => (
+              <label key={color.id} className="flex items-center space-x-2">
+                <input
+                  type="checkbox"
+                  value={color.name}
+                  className="checkbox checkbox-sm"
+                  checked={getParamValues("colors").includes(color.name)}
+                  onChange={() => handleItemChange("colors", color.name)}
+                />
+                <span className="w-4 h-4 rounded-full" style={{ backgroundColor: color.hex }}></span>
+                <span>{color.name}</span>
+              </label>
+            ))}
+        </div>
+      )}
     </div>
   );
 };
