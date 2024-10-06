6e1f5e03fb0c4389cb46bca87cb6ee3f6f11c446
IAmAnshul07 <anshulpatidar1298@gmail.com>
2024-10-06 17:12:22 +0530

    fixed duplicate image upload

--- src/components/add-image/index.tsx
+++ src/components/add-image/index.tsx
@@ -13,6 +13,7 @@ interface Image {
   id: string;
   src: string;
   index: number;
+  fileName: string;
 }
 
 interface ImageUploaderProps {
@@ -30,6 +31,9 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ onImagesUploaded }) => {
 
     if (files) {
       const validFiles: File[] = Array.from(files).filter((file) => {
+        // const fileIndex = array.findIndex((duplicateFilterFile) => duplicateFilterFile.name == file.name);
+
+        // if (fileIndex != index) return false;
         if (file.size > MAX_FILE_SIZE) {
           setFileSizeError(true);
           setTimeout(() => setFileSizeError(false), 3000);
@@ -46,7 +50,7 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ onImagesUploaded }) => {
       const convertToBase64 = (file: File, index: number): Promise<Image> => {
         return new Promise((resolve, reject) => {
           const reader = new FileReader();
-          reader.onload = () => resolve({ id: `${Date.now()}_${file.name}`, src: reader.result as string, index });
+          reader.onload = () => resolve({ id: `${Date.now()}_${file.name}`, src: reader.result as string, index, fileName: file.name });
           reader.onerror = (error) => reject(error);
           reader.readAsDataURL(file);
         });
