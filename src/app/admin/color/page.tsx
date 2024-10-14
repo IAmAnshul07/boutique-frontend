6e0305781720e9f059b6a6dba1a1315a9eeab60f
IAmAnshul07 <anshulpatidar1298@gmail.com>
2024-10-15 04:14:04 +0530

    Added toasts for Color section in admin

--- src/app/admin/color/page.tsx
+++ src/app/admin/color/page.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useGetColorsQuery, useDeleteColorMutation, useCreateColorMutation, useUpdateColorMutation } from "@/redux/services/color";
 import Table from "@/components/table";
 import ColorModal from "@/components/modal/color-modal";
+import { toast } from "react-toastify";
 
 const ColorsSection: React.FC = () => {
   const [page, setPage] = useState<number>(1);
@@ -11,10 +12,8 @@ const ColorsSection: React.FC = () => {
   const [deleteColor] = useDeleteColorMutation();
   const [createColor] = useCreateColorMutation();
   const [updateColor] = useUpdateColorMutation();
-  const [deleteToast, setDeleteToast] = useState<boolean>(false);
   const [selectedColor, setSelectedColor] = useState<any>(null);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const [successToast, setSuccessToast] = useState<string | null>(null);
 
   useEffect(() => {
     if (data && data.result.length === 0 && page > 1) {
@@ -25,8 +24,11 @@ const ColorsSection: React.FC = () => {
   const handleDelete = async (colorId: number) => {
     try {
       await deleteColor(colorId);
-      setDeleteToast(true);
-      setTimeout(() => setDeleteToast(false), 3000);
+      toast.promise(deleteColor, {
+        success: "Color deleted successfully",
+        pending: "Deleting color...",
+        error: "Error deleting color",
+      });
     } catch (error) {
       console.error("Error Deleting Color!!", error);
     }
@@ -49,16 +51,18 @@ const ColorsSection: React.FC = () => {
     try {
       if (selectedColor) {
         await updateColor({ id: selectedColor.id, data: { name: colorName, hex: colorHex.toUpperCase() } });
-        setSuccessToast("Color updated successfully");
-        setTimeout(() => {
-          setSuccessToast(null);
-        }, 3000);
+        toast.promise(updateColor, {
+          success: "Color updated successfully",
+          pending: "Updating color...",
+          error: "Error updating color",
+        });
       } else {
         await createColor({ data: { name: colorName, hex: colorHex.toUpperCase() } });
-        setSuccessToast("Color added successfully");
-        setTimeout(() => {
-          setSuccessToast(null);
-        }, 3000);
+        toast.promise(createColor, {
+          success: "Color added successfully",
+          pending: "Adding color...",
+          error: "Error adding color",
+        });
       }
       setSelectedColor(null);
       setIsModalOpen(false);
@@ -79,20 +83,6 @@ const ColorsSection: React.FC = () => {
   return (
     <>
       <div className="flex flex-col w-full">
-        {deleteToast && (
-          <div className="toast toast-center toast-middle">
-            <div className="alert bg-red">
-              <span className="text-white">Color deleted successfully.</span>
-            </div>
-          </div>
-        )}
-        {successToast && (
-          <div className="toast toast-center toast-middle">
-            <div className="alert success-info bg-green-500">
-              <span>{successToast}</span>
-            </div>
-          </div>
-        )}
         <div className="flex justify-between items-center mx-2 mt-5 h-15 rounded-lg">
           <h1 className="font-semibold text-md mx-5 text-4xl">Colors</h1>
           <button onClick={() => setIsModalOpen(true)} className="btn btn-primary">
@@ -110,15 +100,17 @@ const ColorsSection: React.FC = () => {
                 <p className="text-center text-2xl">No colors found</p>
               </div>
             )}
-            <div className="join flex justify-center">
-              <button className="join-item btn" onClick={prevPage} disabled={page === 1}>
-                «
-              </button>
-              <button className="join-item btn">Page {page}</button>
-              <button className="join-item btn" onClick={nextPage} disabled={page >= Math.ceil(data?.total / 10)}>
-                »
-              </button>
-            </div>
+            {data.result.length && (
+              <div className="join flex justify-center">
+                <button className="join-item btn" onClick={prevPage} disabled={page === 1}>
+                  «
+                </button>
+                <button className="join-item btn">Page {page}</button>
+                <button className="join-item btn" onClick={nextPage} disabled={page >= Math.ceil(data?.total / 10)}>
+                  »
+                </button>
+              </div>
+            )}
           </div>
         </div>
 
